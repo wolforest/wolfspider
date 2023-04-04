1652ff545ba3f086efca4cc497dbe8a53361612d
winglechen <winglechen@gmail.com>
2023-04-04 14:52:08 +0800

    feature: add html parser

---
+++ wolfspider/core/html.py
@@ -0,0 +1,10 @@
+
+class HtmlParser(object):
+
+    def parse(self, text: str, selector: dict) -> dict:
+        """
+        :param text: str
+        :param selector: dict
+        :return: dict {}
+        """
+        return {}
